a84d4713c39435febc2fa867ad0679a312a848d2
erdenebayarotgonbayar14-web <erdenebayar.otgonbayar14@gmail.com>
2026-08-13 13:36:49 +0800

    Төслийн мэдээлэл шинэчлэв

--- data.js
+++ data.js
@@ -5,12 +5,67 @@ const SITE = {
   "name": "Erdenebayar",
   "role": "Photography",
   "place": "Ulaanbaatar, Mongolia",
-  "email": "",
-  "phone": "",
+  "email": "erdenebayar.otgonbayar14@gmail.com",
+  "phone": "+976 95341282",
   "instagram": "@werdbay",
   "instagramUrl": "https://www.instagram.com/werdbay/",
-  "instagramLabel": "Instagram руу очих",
+  "instagramLabel": "Instagram-аар зочлох",
   "intro": ""
 };
 
-const ALBUMS = [];
+const ALBUMS = [
+  {
+    "slug": "film",
+    "title": "Хальс 2026",
+    "year": "2026",
+    "place": "Mongolia",
+    "cover": "photos/film/cover-msr34tek.jpg",
+    "description": "2026 онд дарсан хальсны зурагнууд",
+    "photos": [
+      {
+        "src": "photos/film/17filmlab-201-msr34vgw.jpg",
+        "caption": ""
+      },
+      {
+        "src": "photos/film/17filmlab-207-msr34x0j.jpg",
+        "caption": ""
+      },
+      {
+        "src": "photos/film/17filmlab-202-msr34yyx.jpg",
+        "caption": ""
+      },
+      {
+        "src": "photos/film/17filmlab-213-msr3524o.jpg",
+        "caption": ""
+      },
+      {
+        "src": "photos/film/17filmlab-214-msr353jx.jpg",
+        "caption": ""
+      },
+      {
+        "src": "photos/film/17filmlab-220-msr35557.jpg",
+        "caption": ""
+      },
+      {
+        "src": "photos/film/17filmlab-205-msr356zp.jpg",
+        "caption": ""
+      },
+      {
+        "src": "photos/film/17filmlab-225-msr358vm.jpg",
+        "caption": ""
+      },
+      {
+        "src": "photos/film/17filmlab-226-msr35c5k.jpg",
+        "caption": ""
+      },
+      {
+        "src": "photos/film/17filmlab-227-msr35kwl.jpg",
+        "caption": ""
+      },
+      {
+        "src": "photos/film/17filmlab-228-msr35nim.jpg",
+        "caption": ""
+      }
+    ]
+  }
+];
